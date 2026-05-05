9c2cbe5ca65d28894bbe0af8df3b2c8f86ba8203
kaka <kakaperpetuo2001@gmail.com>
2026-05-05 17:48:42 -0300

    Conclusao do dia 37

---
+++ Dia 37/main.py
@@ -0,0 +1,66 @@
+import requests
+from datetime import datetime
+
+USERNAME = "kakaperpetuo2001"
+TOKEN = "dkfhskdhfuhsidh"
+GRAPH_ID = "graph2"
+
+pixela_endpoint = "https://pixe.la/v1/users"
+
+user_params = {
+    "token": TOKEN,
+    "username" : USERNAME,
+    "agreeTermsOfService" : "yes",
+    "notMinor" : "yes"
+}
+
+#response = requests.post(url=pixela_endpoint, json=user_params)
+#print(response.text)
+
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs" 
+
+graph_config = {
+    "id": GRAPH_ID,
+    "name": "Reading pages",
+    "unit": "Pages",
+    "type": "float",
+    "color": "ajisai"
+}
+
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+
+#response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+#print(response.text)
+
+pixel_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
+
+today = datetime.now()
+
+pixel_config = {
+    "date": today.strftime("%Y%m%d"),
+    "quantity": "20",
+}
+
+#response = requests.post(url=pixel_endpoint, json=pixel_config, headers=headers)
+#print(response.text)
+
+update_pixel_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{today.strftime('%Y%m%d')}"
+
+update_pixel_config = {
+    "quantity": "10",
+}
+
+#response = requests.put(url=update_pixel_endpoint, json=update_pixel_config, headers=headers)
+#print(response.text)
+
+delete_pixel_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{today.strftime('%Y%m%d')}"
+
+#response = requests.delete(url=delete_pixel_endpoint, headers=headers)
+#print(response.text)
+
+
+
+
+
